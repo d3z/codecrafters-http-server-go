2d8f7d713bc4e1bf4edacb7985ab840fb17efd95
Gareth Fleming <gareth@joocy.com>
2024-11-22 15:40:07 +0000

    Extract URL Path

--- app/server.go
+++ app/server.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+type Request struct {
+	Method string
+	Path   string
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -23,5 +29,25 @@ func main() {
 		os.Exit(1)
 	}
 
+	requestStr := make([]byte, 1024)
+	_, err = conn.Read(requestStr)
+
+	request := parseRequest(requestStr)
+
+	if request.Path != "/" {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+	}
+
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
+
+func parseRequest(request []byte) Request {
+	requestLines := strings.Split(string(request), "\r\n")
+	requestLine := requestLines[0]
+	requestLineParts := strings.Split(requestLine, " ")
+	return Request{
+		Method: requestLineParts[0],
+		Path:   requestLineParts[1],
+	}
 }
