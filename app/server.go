8369b30295ced8ed39b83146e96a718895dda1db
Gareth Fleming <gareth@joocy.com>
2024-11-24 15:19:44 +0000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -11,19 +13,29 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+var fileRoot string
+
 type Path struct {
 	FullPath string
-	Parts    []string
+	PathParameters    []string
 }
 
 type Request struct {
 	Method string
 	Path   Path
-	Command string
 	Headers map[string]string
 }
 
+type Response struct {
+	Status int
+	Headers map[string]string
+	Body []byte
+}
+
 func main() {
+	flag.StringVar(&fileRoot, "directory", ".", "The directory to serve files from")
+	flag.Parse()
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -47,37 +59,94 @@ func handleRequest(conn net.Conn) {
 
 	request := parseRequest(requestStr)
 
-	if request.Command == "echo" {
-		writeOKResponse(conn, request.Path.Parts[1])
-	} else if request.Command == "user-agent" {
+	if request.Path.PathParameters[0] == "echo" {
+		writeOKResponse(conn, []byte(request.Path.PathParameters[1]))
+	} else if request.Path.PathParameters[0] == "user-agent" {
 		useragent := request.Headers["User-Agent"]
-		writeOKResponse(conn, useragent)
+		writeOKResponse(conn, []byte(useragent))
+	} else if request.Path.PathParameters[0] == "files" {
+		writeFileResponse(conn, request.Path.PathParameters)
 	} else if request.Path.FullPath == "/" {
-		writeOKResponse(conn, "HTTP/1.1 200 OK\r\n")
+		writeOKResponse(conn, []byte("HTTP/1.1 200 OK\r\n"))
 	} else {
-		writeResponse(conn, 404, "Not Found")
+		writeNotFoundResponse(conn)
 	}
 }
 
-func writeOKResponse(conn net.Conn, response string) {
-	writeResponse(conn, 200, response)
+func writeOKResponse(conn net.Conn, body []byte) {
+	headers := make(map[string]string)
+	headers["Content-Type"] = "text/plain"
+	headers["Content-Length"] = fmt.Sprintf("%d", len(body))
+	response := Response {
+		Status: 200,
+		Headers: headers,
+		Body: body,
+	}
+	writeResponse(conn, response)
 }
 
-func writeResponse(conn net.Conn, status int, response string) {
-	writeStatusLine(conn, status)
-	writeHeader(conn, "Content-Type", "text/plain")
-	writeHeader(conn, "Content-Length", fmt.Sprintf("%d", len(response)))
-	conn.Write([]byte(fmt.Sprintf("\r\n%s", response)))
+func writeNotFoundResponse(conn net.Conn) {
+	response := Response {
+		Status: 404,
+		Headers: make(map[string]string),
+		Body: []byte("404 Not Found"),
+	}
+	writeResponse(conn, response)
+}
+
+func writeErrorResponse(conn net.Conn) {
+	response := Response {
+		Status: 400,
+		Headers: make(map[string]string),
+		Body: []byte("Bad request"),
+	}
+	writeResponse(conn, response)
+}
+
+func writeResponse(conn net.Conn, response Response) {
+	writeStatusLine(conn, response.Status)
+	for header, value := range response.Headers {
+		writeHeader(conn, header, value)
+	}
+	if response.Headers["Content-Length"] == "" {
+		writeHeader(conn, "Content-Length", fmt.Sprintf("%d", len(response.Body)))
+	}
+	conn.Write([]byte("\r\n"))
+	conn.Write(response.Body)
 }
 
 func writeStatusLine(conn net.Conn, statusCode int) {
 	conn.Write([]byte(fmt.Sprintf("HTTP/1.1 %s\r\n", lineForStatusCode(statusCode))))
 }
 
+func writeFileResponse(conn net.Conn, args []string) {
+	if len(args) != 2 {
+		writeErrorResponse(conn)
+		return
+	}
+	filename := args[1]
+	filePath := fmt.Sprintf("%s/%s", fileRoot, filename)
+	content, err := os.ReadFile(filePath)
+	if errors.Is(err, os.ErrNotExist) {
+		writeNotFoundResponse(conn)
+	}
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/octet-stream"
+	headers["Content-Length"] = fmt.Sprintf("%d", len(content))
+	response := Response{
+		Status: 200,
+		Headers: headers,
+		Body: content,
+	}
+	writeResponse(conn, response)
+}
+
 func lineForStatusCode(statusCode int) string {
 	switch statusCode {
 	case 200:
 		return "200 OK"
+	case 400:
+		return "400 Bad Request"
 	case 404:
 		return "404 Not Found"
 	default:
@@ -98,7 +167,6 @@ func parseRequest(requestStrings []byte) Request {
 		Method: requestLineParts[0],
 		Path:   parsePath(requestLineParts[1]),
 	}
-	request.Command = request.Path.Parts[0]
 
 	if len(requestLines) > 1 {
 		request.Headers = parseHeaders(requestLines[1:])
@@ -111,7 +179,7 @@ func parsePath(path string) Path {
 	pathParts := strings.Split(path, "/")
 	return Path{
 		FullPath: path,
-		Parts:    pathParts[1:],
+		PathParameters:    pathParts[1:],
 	}
 }
 
